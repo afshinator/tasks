7435da10dc349d6f2c5fed76741b70d275c36e13
Afshin Mokhtari <afshinator@hotmail.com>
2017-11-08 21:56:58 -0800

    found more to destructure props

--- src/TasksList.js
+++ src/TasksList.js
@@ -3,17 +3,17 @@ import Task from "./Task";
 
 class TasksList extends Component {
   render() {
-    const props = this.props;
-    if (!props.tasks) return null;
+    const { tasks, trashClickHandler, taskEditHandler } = this.props;
+    if (!tasks) return null;
 
-    return props.tasks.map((task, i) => {
+    return tasks.map((task, i) => {
       return (
         <Task
           key={i}
           index={i}
-          content={props.tasks[i]}
-          trashClickHandler={props.trashClickHandler.bind(this, i)}
-          taskEditHandler={props.taskEditHandler}
+          content={tasks[i]}
+          trashClickHandler={trashClickHandler.bind(this, i)}
+          taskEditHandler={taskEditHandler}
         />
       );
     });
